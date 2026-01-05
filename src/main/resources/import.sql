f0427e34fbe834b7a250caba2bd00bbedd1f76df
Adriano <adriano.luis.machado2013@hotmail.com>
2026-01-05 19:32:34 -0300

    Desafio: Implementando Lombok e repositorio de restaurantes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
-INSERT INTO cozinha (nome) VALUES ('Tailandesa');
-INSERT INTO cozinha (nome) VALUES ('Indiana');
+--INSERT INTO cozinha (nome) VALUES ('Tailandesa');
+--INSERT INTO cozinha (nome) VALUES ('Indiana');
+
+INSERT INTO restaurante (nome, taxa_frete) VALUES ('Casa Velha', 0.7);
+INSERT INTO restaurante (nome, taxa_frete) VALUES ('Outback', 0.8);
+INSERT INTO restaurante (nome, taxa_frete) VALUES ('O Camarão', 0.9);
