878a74629036273a06801dd1b4935273f6da456c
Adriano <adriano.luis.machado2013@hotmail.com>
2026-01-07 18:49:29 -0300

    Modelando e implementando a inclusao de recursos com POST

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,14 +3,18 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
 //import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 //import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -52,6 +56,12 @@ public class CozinhaController {
 //		HttpHeaders headers = new HttpHeaders();
 //		headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas"); // response status 302
 //		return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.adicionar(cozinha);
 	}
 	
 
