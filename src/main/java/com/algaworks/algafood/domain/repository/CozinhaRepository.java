3422cb4ba804924c141ebb9608c9412681bf3d38
Adriano <adriano.luis.machado2013@hotmail.com>
2026-01-09 19:42:46 -0300

    Usando as keywords para definir criterios de query methods

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -11,7 +11,8 @@ import com.algaworks.algafood.domain.model.Cozinha;
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long>{ 
 	
-	List<Cozinha> findTodasByNome(String nome);
+//	List<Cozinha> findTodasByNome(String nome);
+	List<Cozinha> findTodasByNomeContaining(String nome);
 	
 	Optional<Cozinha> findByNome(String nome);
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,8 +2,8 @@ INSERT INTO cozinha (nome) VALUES ('Tailandesa');
 INSERT INTO cozinha (nome) VALUES ('Indiana');
 
 INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Casa Velha', 0.7, 1);
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Outback', 0.8, 2);
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('O Camarão', 0.9, 1);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Outback', 1.2, 2);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('O Camarão', 4.5, 1);
 
 INSERT INTO estado (nome) VALUES ('Estado ABC');
 INSERT INTO estado (nome) VALUES ('Estado DEF');
