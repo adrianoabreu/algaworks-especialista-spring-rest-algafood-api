565ecbae1505e3970b893cceaeb9885ba48b9e99
Adriano <adriano.luis.machado2013@hotmail.com>
2026-01-12 23:17:47 -0300

    Tornando a consulta com Criteria API com filtros dinamicos

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,11 +1,13 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 //import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 //import org.springframework.util.StringUtils;
+import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
@@ -13,10 +15,10 @@ import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
+//import jakarta.persistence.criteria.CriteriaBuilder;
+//import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
+//import jakarta.persistence.criteria.Root;
 
 //Esta classe precisa ter o sufixo Impl para o Spring Data JPA entender que existe implementação de metodos da interface RestauranteRepository.
 @Repository
@@ -29,18 +31,28 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	public List<Restaurante> find(String nome, 
 			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
 		
-		CriteriaBuilder builder = manager.getCriteriaBuilder();
+//		CriteriaBuilder builder = manager.getCriteriaBuilder();
+//		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+//		Root<Restaurante> root = criteria.from(Restaurante.class); // from Restaurante
 		
-		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-		Root<Restaurante> root = criteria.from(Restaurante.class); // from Restaurante
+		var builder = manager.getCriteriaBuilder();
+		var criteria = builder.createQuery(Restaurante.class);
+		var root = criteria.from(Restaurante.class); // from Restaurante
 		
-		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+		var predicates = new ArrayList<Predicate>();
 		
-		Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+		if(StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));			
+		}
 		
-		Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
-
-		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
+		if(taxaFreteInicial != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+		}
+		if(taxaFreteFinal != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+		}
+		
+		criteria.where(predicates.toArray(new Predicate[0]));
 		
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);		
 		return query.getResultList();
