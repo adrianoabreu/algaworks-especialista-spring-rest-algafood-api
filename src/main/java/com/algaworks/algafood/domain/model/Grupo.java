596b5c84e5cac95df4e80c2f9e7fc4285575aa51
Adriano <adriano.luis.machado2013@hotmail.com>
2026-01-14 17:34:39 -0300

    Desafio: mapeando relacionamentos muitos-para-muitos

--- src/main/java/com/algaworks/algafood/domain/model/Grupo.java
+++ src/main/java/com/algaworks/algafood/domain/model/Grupo.java
@@ -1,17 +1,25 @@
 package com.algaworks.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Permissao {
+public class Grupo {
 
 	@EqualsAndHashCode.Include
 	@Id
@@ -21,8 +29,11 @@ public class Permissao {
 	@Column(nullable = false)
 	private String nome;
 	
-	@Column(nullable = false)
-	private String descricao;
-	
+	@JsonIgnore
+	@ManyToMany
+	@JoinTable(name = "grupo_permissao", 
+			joinColumns = @JoinColumn(name = "grupo_id"), 
+			inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+	private List<Permissao> permissoes = new ArrayList<>();
 	
 }
