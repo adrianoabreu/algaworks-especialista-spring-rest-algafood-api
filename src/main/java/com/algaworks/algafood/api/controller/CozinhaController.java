f7498b843bc5897d910fd4468bdfb72bbeff0e12
Adriano <adriano.luis.machado2013@hotmail.com>
2026-01-20 17:29:50 -0300

    Simplificando o codigo com o uso de @ResponseStatus em exceptions

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -1,7 +1,7 @@
 package com.algaworks.algafood.api.controller;
 
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 //import org.springframework.http.HttpStatus;
 //import org.springframework.http.HttpStatus;
 //import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,20 +49,8 @@ public class CozinhaController {
 	
 //	@ResponseStatus(HttpStatus.OK) // alterando codigo status response http. 
 	@GetMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {	
-		Optional<Cozinha> cozinha = cozinhaRepository.findById(cozinhaId);
-		
-		//ResponseEntity permite customizar a resposta HTTP com status e corpo(payload) do retorno da requisição.
-
-		if(cozinha.isPresent()) {
-//			return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-			return ResponseEntity.ok(cozinha.get());			
-		}
-
-		return ResponseEntity.notFound().build();			
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas"); // response status 302
-//		return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+	public Cozinha buscar(@PathVariable Long cozinhaId) {	
+		return cadastroCozinha.buscarOuFalhar(cozinhaId);
 	}
 	
 	@PostMapping
@@ -72,32 +60,13 @@ public class CozinhaController {
 	}
 	
 	@PutMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
-		Optional<Cozinha> cozinhaAtual = cozinhaRepository.findById(cozinhaId);
+	public Cozinha atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtual = cadastroCozinha.buscarOuFalhar(cozinhaId);
 		
-		if(cozinhaAtual != null) {
-//		   cozinhaAtual.setNome(cozinha.getNome());
-		   BeanUtils.copyProperties(cozinha, cozinhaAtual.get(), "id"); // copia os dados dos atributos de cozinha para cozinhaAtual
+		BeanUtils.copyProperties(cozinha, cozinhaAtual, "id"); // copia os dados dos atributos de cozinha para cozinhaAtual
 		
-	       Cozinha cozinhaSalva = cadastroCozinha.salvar(cozinhaAtual.get());
-		   return ResponseEntity.ok(cozinhaSalva);
-		}
-		return ResponseEntity.notFound().build();
+	    return cadastroCozinha.salvar(cozinhaAtual);
 	}
-
-// 	@DeleteMapping("/{cozinhaId}")
-//	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
-//		try {
-//			cadastroCozinha.excluir(cozinhaId);
-//			return ResponseEntity.noContent().build();
-//		
-//		} catch (EntidadeNaoEncontradaException e) {
-//			return ResponseEntity.notFound().build();
-//			
-//		} catch (EntidadeEmUsoException e) {
-//			return ResponseEntity.status(HttpStatus.CONFLICT).build(); // Codigo response status HTTP 409.
-//		}
-//	}
 	
 	@DeleteMapping("/{cozinhaId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
